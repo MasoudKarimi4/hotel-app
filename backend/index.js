f6f534eea98dca84f20e4b7213dce03f3bc2c0f7
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2024-02-19 15:35:34 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 // Const is a variable that cannot be changed 
 
 
+// Importing the required libraries / packages
+// Express is the backend framework
+// Cors is needed 
 const express = require('express');
 const app = express();
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000')
