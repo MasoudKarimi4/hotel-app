d3edce1f1bb0b2b860438b6120a040dff5324383
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2024-02-19 15:30:11 -0500

    fixed it

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+// Const is a variable that cannot be changed 
+
+
+const express = require('express');
+const app = express();
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000')
+})
+
