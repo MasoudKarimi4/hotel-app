4ba597161b8c0ef62d8831397a4805599b6fe265
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2024-02-20 14:24:02 -0500

    restful apis

--- backend/index.js
+++ backend/index.js
@@ -40,11 +40,59 @@ app.post("/todos", async (req, res) => {
 
 // get all todos
 
+app.get("/todos", async (req, res) => { 
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
 // get a todo
+app.get("/todos/:id", async (req, res) => { 
+    try {
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * from todo WHERE todo_id = $1", [id])
+        res.json(todo.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: "Server error" });
+    }
+});
 
 //update a todo
 
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {description} = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",[description, id])
+
+        res.json("Todo was updated!")
+        
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: "Server error" });
+    }
+
+});
+
+
 // delete a todo 
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+
+        res.json("Todo was deleted!");
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000')
