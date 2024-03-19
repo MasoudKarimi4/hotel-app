a8c99af91c1cfb35cb1ed0880d0856af84e0c633
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2024-03-18 22:32:17 -0400

    Input form connect to db

--- backend/index.js
+++ backend/index.js
@@ -95,6 +95,6 @@ app.delete("/todos/:id", async (req, res) => {
 
 
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000')
+    console.log('Server is listening on port 5000e')
 })
 
